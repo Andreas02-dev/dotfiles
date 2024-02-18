bfd230cbabaf0815ab29937e9d8ac6b6ba643760
Andreas Hoornstra <andreashoornstra@gmail.com>
2024-02-18 23:33:50 +0100

    `Boot` | Improve boot process

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -38,9 +38,25 @@ nixpkgs.overlays = [
     options = "--delete-older-than +5";
   };
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Custom 'timeout' to ensure Plymouth boot screen is shown, disable for now
+  # services.xserver.displayManager.job.preStart = "sleep 2";
+
+  # Boot
+  boot =
+  {
+    # Plymouth
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    plymouth.enable = true;
+    kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
+    # Boot Loader
+    loader =
+    {
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
+  };
 
   networking.hostName = "xps"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
