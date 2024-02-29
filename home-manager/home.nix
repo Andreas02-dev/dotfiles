e0bedfedf81ea87c29d0dabd871e1147bcb04962
Andreas Hoornstra <andreashoornstra@gmail.com>
2024-02-29 23:00:04 +0100

    `Fix` | Fix ssh-config permission issue in FHS environments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -107,7 +107,12 @@ in
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".ssh/config".source = ../ssh/config;
+    # ".ssh/config".source = ../ssh/config;
+    ".ssh/config" = {
+      source = ../ssh/config;
+      target = ".ssh/config_source";
+      onChange = "cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config";
+    };
     ".config/onlyoffice/DesktopEditors.conf".source = ../onlyoffice/DesktopEditors.conf;
     ".local/share/onlyoffice/desktopeditors/data/settings.xml".source = ../onlyoffice/settings.xml;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
