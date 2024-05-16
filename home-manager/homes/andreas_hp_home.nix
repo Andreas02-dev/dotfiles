28ececfe0d3884a46e53c9e42023fb3d3bfc64d4
Andreas Hoornstra <andreashoornstra@gmail.com>
2024-05-16 15:46:39 +0200

    `Add` | Add `Zotero` to HP

--- home-manager/homes/andreas_hp_home.nix
+++ home-manager/homes/andreas_hp_home.nix
@@ -43,6 +43,7 @@
     direnv
     subtitleedit
     vesktop
+    zotero
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
