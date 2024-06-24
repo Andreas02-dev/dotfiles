1bcf5172837dc4f5fc50df36959ed110cfec65c8
Andreas Hoornstra <andreashoornstra@gmail.com>
2024-06-24 12:29:27 +0200

    `Improvement` | Hide dummy input camera

--- etc/nixos/ipu6.nix
+++ etc/nixos/ipu6.nix
@@ -38,4 +38,21 @@ in
   hardware.firmware = [
     ivsc-firmware
   ];
+
+  # https://discourse.nixos.org/t/how-to-hide-this-dummy-video-device/40985/5
+  services.udev.extraRules = ''
+    # If the system is not a video device, we skip these rules by jumping to the end
+    SUBSYSTEM!="video4linux", GOTO="hide_cam_end"
+    #ATTR{name}=="Intel MIPI Camera", GOTO="hide_cam_end" # This line cannot be used as it would move too much stuff and then the camera would not work. Instead, we just move the dummy camera,
+    # I found its name with udevadm info -q all -a /dev/video0
+    # If this is not the dummy video, we also skip these rules.
+    ATTR{name}!="Dummy video device (0x0000)", GOTO="hide_cam_end"
+    ACTION=="add", RUN+="${pkgs.coreutils}/bin/mkdir -p /dev/not-for-user"
+    ACTION=="add", RUN+="${pkgs.coreutils}/bin/mv -f $env{DEVNAME} /dev/not-for-user/"
+
+    ACTION=="remove", RUN+="${pkgs.coreutils}/bin/rm -f /dev/not-for-user/$name"
+    ACTION=="remove", RUN+="${pkgs.coreutils}/bin/rm -f /dev/not-for-user/$env{ID_SERIAL}"
+
+    LABEL="hide_cam_end"
+  '';
 }
