b647ba894eedd255de9203d0c33a9cb6315c5eda
Andreas Hoornstra <Andreas.Hoornstra@infosupport.com>
2025-09-12 11:05:07 +0200

    feat(hm-nixos): add nano configuration file

--- home-manager/homes/nixos_nixos_home.nix
+++ home-manager/homes/nixos_nixos_home.nix
@@ -93,6 +93,10 @@ in
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".nanorc".text = ''
+      set constantshow       # show line/column number
+      set guidestripe 80     # vertical ruler at column 80
+    '';
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
