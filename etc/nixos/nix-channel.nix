b21a6a9b68bc912072bb0b806e37d59fc04b2a37
Andreas Hoornstra <andreashoornstra@gmail.com>
2025-11-23 11:23:35 +0100

    fix(nix-channel-compat): fix nix channel compat for some tools
    some tools ignore paths starting with `/etc`, so work around this
    by adding a custom path in the `/nix` directory structure
    one of the tools that ignores NIX_PATH paths starting with `/etc`
    is VS Code's rust_analyzer extension

--- etc/nixos/nix-channel.nix
+++ etc/nixos/nix-channel.nix
@@ -1,16 +1,21 @@
 {
-  config,
-  pkgs,
-  inputs,
-  ...
-}: let
-  nixPath = "/etc/nixPath";
-in {
-  systemd.tmpfiles.rules = [
-    "L+ ${nixPath} - - - - ${pkgs.path}"
-  ];
+  config
+, pkgs
+, inputs
+, ...
+}: 
+  let
+    # Previously /etc/nixPath, but some tools
+    # (f.e. VS Code extension rust-analyzer)
+    # Ignore /etc/ paths
+    nixPath = "/nix/var/nix/nixPath/nixpkgs"; 
+  in {
+    systemd.tmpfiles.rules = [
+      "d /nix/var/nix/nixPath 0755 root root -"
+      "L+ ${nixPath} - - - - ${pkgs.path}"
+    ];
 
-  nix = {
-    nixPath = ["nixpkgs=${nixPath}"];
-  };
+    nix.nixPath = [
+      "nixpkgs=${nixPath}"
+    ];
 }
